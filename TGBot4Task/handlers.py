d391619e931e756d550be36c6f2f476d6ba36a60
DonaldGubin <bassboosted111@mail.ru>
2024-10-10 20:04:36 +0300

    'last'

--- TGBot4Task/handlers.py
+++ TGBot4Task/handlers.py
@@ -1,3 +1,4 @@
+import re
 from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandObject
@@ -6,6 +7,8 @@ import requests
 router = Router()
 
 ITEMS_PER_PAGE = 100
+
+SEARCH_PATTERN = re.compile(r"(.+)\s(\d+)$")  
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
@@ -16,89 +19,57 @@ async def start_handler(msg: Message):
 
 @router.message(Command("search"))
 async def start_handler_search(msg: Message, command: CommandObject):
-    info = command.args
-    query = info.split(' ')[0]  # категория
-    article = info.split(' ')[-1]  # артикул
+    try:
+        info = command.args
+        if not info:
+            raise ValueError("Отсутствуют аргументы. Введите запрос в формате: /search Категория Артикул")
 
-    # артикул состоит только из цифр
-    if not article.isdigit():
-        await msg.answer("Неправильный формат артикула. Пожалуйста, введите только цифры.")
-        return
+        match = SEARCH_PATTERN.match(info)
+        if not match:
+            raise ValueError(
+                "Неверный формат запроса. Введите запрос в формате: /search Категория Артикул\nПример: /search Платье 87989388")
 
-    found = False
-    article = int(article)
+        query, article = match.groups()
 
-    # 50 страниц (по 100 товаров на каждой)
-    for page in range(1, 51):
-        response = requests.get(
-            f'https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=123586098&query={query}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false&page={page}'
-        )
-        products = response.json().get('data', {}).get('products', [])
+        if not article.isdigit():
+            raise ValueError("Неправильный формат артикула. Пожалуйста, введите только цифры.")
 
-        if not products:
-            await msg.answer("Товар не найден.")
-            return
+        found = False
+        article = int(article)
 
-        for index, product in enumerate(products):
-            if product.get('id') == article:
-                position = (page - 1) * ITEMS_PER_PAGE + (index + 1)  # Общая позиция товара
-                await msg.answer(
-                    f"Товар найден на странице {page}, на {index + 1}-м месте (общая позиция: {position}).")
-                found = True
+        # 50 страниц (по 100 товаров на каждой)
+        for page in range(1, 51):
+            response = requests.get(
+                f'https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=123586098&query={query}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false&page={page}'
+            )
+            products = response.json().get('data', {}).get('products', [])
+
+            if not products:
+                await msg.answer("Товар не найден.")
+                return
+
+
+            for index, product in enumerate(products):
+                if product.get('id') == article:
+                    position = (page - 1) * ITEMS_PER_PAGE + (index + 1)  # Общая позиция
+                    await msg.answer(
+                        f"Товар найден на странице {page}, на {index + 1}-м месте (общая позиция: {position}).")
+                    found = True
+                    break
+
+            if found:
                 break
 
-        if found:
-            break
+        if not found:
+            await msg.answer("Товар не найден в пределах 50 страниц.")
 
-    if not found:
-        await msg.answer("Товар не найден в пределах 50 страниц.")
+    except ValueError as e:
+        await msg.answer(f"Ошибка: {e}")
+    except Exception as e:
+        await msg.answer(f"Произошла ошибка: {e}")
+
 
 @router.message()
 async def message_handler(msg: Message):
-    await msg.answer("Я предназначен только для поискаа товаров\n"
+    await msg.answer("Я предназначен только для поиска товаров\n"
                      "Напиши команду /start")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     info = command.args
-#     await msg.answer(f"Нашел")
-#     response = requests.get(
-#         f'https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=123586098&query={info}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false')
-#     products = response.json().get('data').get('products')
-#     for product in products:
-#         if product.get('id') == int(info.split(' ')[-1]):
-#             await msg.answer(f'вот расположение {product}')
-#     print(response.json())
-#
-#
-# @router.message()
-# async def message_handler(msg: Message):
-#     await msg.answer("Я предназначен только для поискаа товаров\n"
-#                      "Напиши команду /start")
-
--- TGBot4Task/main.py
+++ TGBot4Task/main.py
@@ -2,15 +2,13 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-
-import config
+from config_reader import config
 from handlers import router
 
+bot = Bot(config.bot_token.get_secret_value())
 
 async def main():
-    bot = Bot(token=config.BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
